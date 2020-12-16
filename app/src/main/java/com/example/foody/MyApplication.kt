72251d701c814fa5a32df8e0a91938454c0655e1
Damian9696 <damian.ogorek.1996@gmail.com>
2020-12-17 00:13:23 +0100

    Updated libraries version; Updated MyApplication class Hilt annotation; Created binding method to load data from url; Added hilt annotations to MainActivity and RecipesFragment; Connected api data with adapter by execute api request, by view model observator retrived data, handle network result.

--- app/src/main/java/com/example/foody/MyApplication.kt
+++ app/src/main/java/com/example/foody/MyApplication.kt
@@ -2,7 +2,8 @@ package com.example.foody
 
 import android.app.Application
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
-@AndroidEntryPoint
+@HiltAndroidApp
 class MyApplication : Application() {
 }
