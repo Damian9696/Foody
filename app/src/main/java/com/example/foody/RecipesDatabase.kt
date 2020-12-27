f25db5802aba2903f1c6666109a2a0d924b5d2c5
Damian9696 <damian.ogorek.1996@gmail.com>
2020-12-27 19:48:38 +0100

    Created the recipe database class and refactored position the database classes.

---
+++ app/src/main/java/com/example/foody/RecipesDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.foody
+
+import androidx.room.Database
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [RecipesEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(RecipesTypeConverter::class)
+abstract class RecipesDatabase {
+
+    abstract fun recipesDao(): RecipesDao
+}
