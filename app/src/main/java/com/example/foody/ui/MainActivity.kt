81b2005764795b9ebae3ec2651dc5457320efbc7
Damian9696 <damian.ogorek.1996@gmail.com>
2021-04-27 22:31:47 +0200

    Added slpsh screen as theme into Android Manifest; Added drawables;

--- app/src/main/java/com/example/foody/ui/MainActivity.kt
+++ app/src/main/java/com/example/foody/ui/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Foody)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navController = findNavController(R.id.navHostFragment)
